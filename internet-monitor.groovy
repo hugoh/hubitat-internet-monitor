c87ab80f65b9d9f6e08e6d44b4b306ef02757a8c
BumpVersion Action <bumpversion@github-actions>
2023-06-08 03:25:11 +0000

    Bump version: 0.4.1 → 0.4.2

--- internet-monitor.groovy
+++ internet-monitor.groovy
@@ -21,7 +21,7 @@ import groovy.transform.Field
 @Field static final String PRESENT_TRUE = 'present'
 @Field static final String PRESENT_FALSE = 'not present'
 
-public static final String version() { return '0.4.1' }
+public static final String version() { return '0.4.2' }
 
 metadata {
     definition(
